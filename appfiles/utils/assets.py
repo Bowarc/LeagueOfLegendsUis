3b40217027c2cb190b2b98a85ba6acb985ec5317
Bowarc <yuyeaaaahhhh@laposte.net>
2025-06-16 20:55:19 +0200

    Fix windows paths

--- appfiles/utils/assets.py
+++ appfiles/utils/assets.py
@@ -3,23 +3,23 @@ import os
 
 class assetsLoader:
     def __init__(self):
-        self.ressourcePath = "appfiles\\src\\"
+        self.ressourcePath = "appfiles/src/"
         pass
 
     def getImage(self, imageName):
         basePath = os.path.abspath(".")
-        relativePath = self.ressourcePath + "img\\" + imageName
+        relativePath = self.ressourcePath + "img/" + imageName
 
         return os.path.join(basePath, relativePath)
 
     def getQss(self, fileName):
         basePath = os.path.abspath(".")
-        relativePath = self.ressourcePath + "qss\\" + fileName
+        relativePath = self.ressourcePath + "qss/" + fileName
 
         return os.path.join(basePath, relativePath)
 
     def getFont(self, fileName):
         basePath = os.path.abspath(".")
-        relativePath = self.ressourcePath + "font\\" + fileName
+        relativePath = self.ressourcePath + "font/" + fileName
 
         return os.path.join(basePath, relativePath)
